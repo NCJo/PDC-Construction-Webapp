49e3c68f3009d62b75847a5970ff4fff365858c7
NCJo <cnimkulr@gmail.com>
2018-12-16 02:06:33 -0700

    Added: Configure urls.py and connect it from urls.py in the project to the app's urls.py

---
+++ pdc_app/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class PdcAppConfig(AppConfig):
+    name = 'pdc_app'
---
+++ pdc_app/urls.py
@@ -0,0 +1,8 @@
+app_name = 'pdc_app'
+from django.urls import path
+from django.conf.urls import include
+from pdc_app import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+]
