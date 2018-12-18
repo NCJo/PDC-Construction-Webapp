21b7a941d9767895eb37caf7d94f9c31941ca7fc
NCJo <cnimkulr@gmail.com>
2018-12-18 01:15:00 -0700

    Added: sample Landing Page

--- PDC/settings.py
+++ PDC/settings.py
@@ -64,7 +64,7 @@ ROOT_URLCONF = 'PDC.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATE_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -128,3 +128,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/dev/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [STATIC_DIR,]
