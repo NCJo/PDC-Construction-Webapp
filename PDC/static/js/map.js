e74435f581450944c027b41fe43616dcf9c55128
NCJo <cnimkulr@gmail.com>
2019-11-19 20:10:21 -0600

    Modified: disable authenticator function in the webapp

--- PDC/static/js/map.js
+++ PDC/static/js/map.js
@@ -1,7 +1,7 @@
 function initMap() {
     var pdcLoc = {lat: 13.804701, lng: 100.417401};
     var map = new google.maps.Map(
-        document.getElementById('map'), {zoom: 15, center: pdcLoc}
+        document.getElementById('map'), {zoom: 15, center: pdcLoc, mapTypeControl: false}
     );
     var marker = new google.maps.Marker({position: pdcLoc, map: map});
 }
--- pdc_app/views.py
+++ pdc_app/views.py
@@ -6,51 +6,51 @@ from django.shortcuts import redirect
 
 # Create your views here.
 
-@login_required
+
 def index(request):
     # Add html landing page
     return render(request, 'pdc_templates/index.html')
 
-@login_required
+
 def projects(request):
     return render(request, 'pdc_templates/projects.html')
 
 # OLD
-@login_required
+
 def samed_gallery(request):
     return render(request, 'pdc_templates/samed.html')
 
-@login_required
+
 def ptb_gallery(request):
     return render(request, 'pdc_templates/ptb_warehouse.html')
 
-@login_required
+
 def cp_gallery(request):
     return render(request, 'pdc_templates/cp.html')
 
-@login_required
+
 def yip_gallery(request):
     return render(request, 'pdc_templates/yip_fac.html')
 
-@login_required
+
 def sangfah_gallery(request):
     return render(request, 'pdc_templates/sangfah.html')
 # END OF OLD
 
 # Projects
-@login_required
+
 def one_gallery(request):
     return render(request, 'pdc_templates/projects/one.html')
 
-@login_required
+
 def two_gallery(request):
     return render(request, 'pdc_templates/projects/two.html')
 
-@login_required
+
 def three_gallery(request):
     return render(request, 'pdc_templates/projects/three.html')
 
-@login_required
+
 def four_gallery(request):
     return render(request, 'pdc_templates/projects/four.html')
 
@@ -120,26 +120,30 @@ def twentyfive_gallery(request):
 def twentysix_gallery(request):
     return render(request, 'pdc_templates/projects/twentysix.html')
 
-
-
-
-
 # End of Projects
 
 
 
-@login_required
+
 def activities(request):
     return render(request, 'pdc_templates/activities.html')
 
-@login_required
+
 def about_us(request):
     return render(request, 'pdc_templates/about_us.html')
 
-@login_required
+
 def contact_us(request):
     return render(request, 'pdc_templates/contact_us.html')
 
 def logout_view(request):
     logout(request)
     return redirect('index')
+
+
+# For login
+
+# @login_required
+# def index(request):
+#     # Add html landing page
+#     return render(request, 'pdc_templates/index.html')
